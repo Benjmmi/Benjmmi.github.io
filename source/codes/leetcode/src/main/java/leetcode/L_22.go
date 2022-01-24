b8dd2b7ab64d64189ae439080f9ae85a2252bcd5
Zhongya Yan <yanzhongya@smzdm.com>
2022-01-24 11:48:19 +0800

    L_2

---
+++ source/codes/leetcode/src/main/java/leetcode/L_22.go
@@ -0,0 +1,47 @@
+package sample
+
+var kv = map[string]int{"()": 1}
+var arr = map[int][]string{1: {"()"}}
+
+func generateParenthesis(n int) []string {
+	if len(arr[n]) > 0 {
+		return arr[n]
+	}
+
+	//insert()
+	for i := 1; i <= n; i++ {
+		if len(arr[i]) > 0 && arr[i][0] != "" {
+			continue
+		}
+		for j := 0; j < len(arr[i-1]); j++ {
+			for k := 0; k < len(arr[i-1][j]); k++ {
+				nk := arr[i-1][j][:k] + "()" + arr[i-1][j][k:]
+				if _, ex := kv[nk]; !ex {
+					kv[nk] = i
+					arr[i] = append(arr[i], nk)
+				}
+				if arr[i-1][j][k] == '(' {
+					pairs := 1
+					for p := k; p < len(arr[i-1][j]); p++ {
+						if arr[i-1][j][p] == '(' {
+							pairs += 1
+						}
+						if arr[i-1][j][p] == '(' {
+							pairs -= 1
+						}
+						if pairs == 0 {
+							ap := arr[i-1][j][:p] + ")" + arr[i-1][j][p:]
+							ap = nk[:k] + "(" + nk[k:]
+							if _, ex := kv[ap]; !ex {
+								kv[ap] = i
+								arr[i] = append(arr[i], ap)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return arr[n]
+}
---
+++ source/codes/leetcode/src/main/java/leetcode/L_22_test.go
@@ -0,0 +1,19 @@
+package sample
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_generateParenthesis(t *testing.T) {
+	fmt.Println(generateParenthesis(1))
+	fmt.Println(generateParenthesis(2))
+	fmt.Println(generateParenthesis(3))
+	fmt.Println(generateParenthesis(4))
+	fmt.Println(generateParenthesis(5))
+	fmt.Println(generateParenthesis(6))
+	fmt.Println(generateParenthesis(7))
+	fmt.Println(generateParenthesis(8))
+	fmt.Println(generateParenthesis(9))
+	fmt.Println(generateParenthesis(10))
+}
