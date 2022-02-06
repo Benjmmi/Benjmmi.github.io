feb81aa5f68fd1c94d4ea2623348ac38a147128c
Zhongya Yan <yan2228598786@gmail.com>
2022-02-06 21:58:49 +0800

    a

--- source/codes/leetcode/src/main/java/leetcode/L_1027.go
+++ source/codes/leetcode/src/main/java/leetcode/L_1027.go
@@ -1,48 +1,78 @@
 package sample
 
-import "fmt"
-
 func longestArithSeqLength(nums []int) int {
-	set := map[int][]int{}
-	for i := 0; i < len(nums); i++ {
-		if set[nums[i]] != nil {
-			set[nums[i]] = append(set[nums[i]], i)
-		} else {
-			set[nums[i]] = []int{i}
+	n, ans := len(nums), 0
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, 1010)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			d := nums[i] - nums[j] + 500
+			dp[i][d] = max(dp[i][d], dp[j][d]+1)
+			ans = max(ans, dp[i][d])
 		}
 	}
-	visited := map[string]int{}
-
-	maxLen := 0
-	for i := 0; i < len(nums); i++ {
-		for k := i + 1; k < len(nums); k++ {
-			rootNum := 1
-			subVal := nums[k] - nums[i]
-			pre := nums[i]
-			for {
-				if v, e := set[pre+subVal]; e {
-					ac := -1
-					for _, index := range v {
-						if index > k {
-							ac = index
-							break
-						}
-					}
-					if ac != -1 {
-						rootNum++
-						visited[fmt.Sprintf("%d_%d_%d", pre, pre+subVal, ac)] = rootNum
-						pre += subVal
-					} else {
-						break
-					}
-				} else {
-					break
-				}
-				if rootNum > maxLen {
-					maxLen = rootNum
-				}
-			}
-		}
-	}
-	return maxLen
+	return ans + 1
 }
+
+//func longestArithSeqLength(nums []int) int {
+//	set := map[int][]int{}
+//	visited := map[string]int{}
+//
+//	for i := 0; i < len(nums); i++ {
+//		if set[nums[i]] != nil {
+//			set[nums[i]] = append(set[nums[i]], i)
+//		} else {
+//			set[nums[i]] = []int{i}
+//		}
+//	}
+//
+//	maxLen := 0
+//	for i := 0; i < len(nums) && len(nums)-i > maxLen; i++ {
+//		for k := i + 1; k < len(nums) && len(nums)-i > maxLen; k++ {
+//			rootNum := 2
+//			last := nums[k]
+//			lastIndex := k
+//			subVal := nums[i] - last
+//			key := strings.Join([]string{strconv.Itoa(lastIndex), strconv.Itoa(subVal)}, "_")
+//			if _, exist := visited[key]; exist {
+//				continue
+//			}
+//			for {
+//				mark := false
+//				last -= subVal
+//				if v, e := set[last]; e {
+//					for i := 0; i < len(v); i++ {
+//						if v[i] > lastIndex {
+//							lastIndex = v[i]
+//							mark = true
+//							break
+//						}
+//					}
+//					if mark {
+//						rootNum++
+//						key := strings.Join([]string{strconv.Itoa(lastIndex), strconv.Itoa(subVal)}, "_")
+//						if total, exist := visited[key]; exist {
+//							if rootNum >= total {
+//								visited[key] = rootNum
+//							} else {
+//								break
+//							}
+//						} else {
+//							visited[key] = rootNum
+//						}
+//					} else {
+//						break
+//					}
+//				} else {
+//					break
+//				}
+//			}
+//			if rootNum > maxLen {
+//				maxLen = rootNum
+//			}
+//		}
+//	}
+//	return maxLen
+//}
