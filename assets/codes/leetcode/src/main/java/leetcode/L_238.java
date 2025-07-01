2804e8d7416b2c9546f10e28da515b15dba82085
yanzhonya <yanzhongya@shizhuang-inc.com>
2025-07-01 20:41:10 +0800

    修改

--- assets/codes/leetcode/src/main/java/leetcode/L_238.java
+++ assets/codes/leetcode/src/main/java/leetcode/L_238.java
@@ -24,19 +24,45 @@ package leetcode;
  */
 public class L_238 {
     public static void main(String[] args) {
-        int[] a = new L_238().productExceptSelf(new int[]{1,2,3,4});
-        for (int i : a) {
-            System.out.println(i);
+        L_238 l238 = new L_238();
+        {
+            int[] a = l238.productExceptSelf(new int[]{1, 2, 3, 4});
+            for (int i : a) {
+                System.out.print(i + ",");
+            }
+            System.out.println("===");
         }
+
+        {
+            int[] a = l238.productExceptSelf(new int[]{-1, 1, 0, -3, 3});
+            for (int i : a) {
+                System.out.print(i + ",");
+            }
+            System.out.println("===");
+        }
+
     }
 
-    int sum = 1;
     public int[] productExceptSelf(int[] nums) {
-        productExceptSelf2(nums,0);
-        return nums;
+        int[] L = new int[nums.length];
+        int[] R = new int[nums.length];
+        int[] answer = new int[nums.length];
+        L[0] = 1;
+        R[nums.length - 1] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
     }
-    public int productExceptSelf2(int[] nums,int index) {
-        nums[index] = productExceptSelf2( nums,index+1);
-        return nums[index];
-    }
+
 }
---
+++ assets/codes/leetcode/src/main/java/leetcode/L_380.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L_380 {
+
+//    public L_380() {
+//
+//    }
+//    List<Integer> list = new ArrayList<>();
+//    public boolean insert(int val) {
+//        list.add(val);
+//        return true;
+//    }
+//
+//    public boolean remove(int val) {
+//        list.remove(val);
+//        return true;
+//    }
+//
+//    public int getRandom() {
+//
+//    }
+}
